11ea0098ff24f58a1e99a43e6efd9f44a91a6baf
Albert Najjar <albertnajjar@outlook.com>
2017-05-16 13:12:17 -0400

    Moved libraries to more convenient location. Added main file with skeleton code, and a shader class to make adding shaders easier.

---
+++ Interactive Room/Interactive Room/Main.cpp
@@ -0,0 +1,87 @@
+#include <stdio.h>
+
+//Shader class to easily add vertex and fragment shader (my object-oriented side is tenacious :P)
+//It already includes GLEW, iostream and string, so no need to include them again.
+#include "Shader.h"
+//GLFW
+#include "..\glfw\glfw3.h"
+//GLM
+#include "..\glm\glm.hpp"
+//Other GLM includes
+#include "..\glm\gtc\matrix_transform.hpp"
+#include "..\glm\gtc\type_ptr.hpp"
+
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 800;
+
+// The MAIN function, from here we start the application and run the game loop
+int main()
+{
+	std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
+	// Init GLFW
+	glfwInit();
+	// Set all the required options for GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// Create a GLFWwindow object that we can use for GLFW's functions
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	// Set the required callback functions
+	glfwSetKeyCallback(window, key_callback);
+
+	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+	glewExperimental = GL_TRUE;
+	// Initialize GLEW to setup the OpenGL Function pointers
+	glewInit();
+
+	// Define the viewport dimensions
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	// Build and compile our shader program
+	// Vertex & fragment shader
+	// Shader myShader("vertex.shader", "fragment.shader");
+
+	// This call will result in wireframe polygons, meaning it will only draw the perimeters of the triangles. Very cool x)
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	// Enable depth test
+	glEnable(GL_DEPTH_TEST);
+	// Accept fragment if it closer to the camera than the former one
+	glDepthFunc(GL_LESS);
+	// Game loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+		glfwPollEvents();
+		// Render
+		// Clear the colorbuffer
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Swap the screen buffers
+		glfwSwapBuffers(window);
+	}
+
+	// Properly de-allocate all resources once they've outlived their purpose
+	// Terminate GLFW, clearing any resources allocated by GLFW.
+	glfwTerminate();
+	return 0;
+}
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	//just for testing. Will remove that later.
+	std::cout << key << std::endl;
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
